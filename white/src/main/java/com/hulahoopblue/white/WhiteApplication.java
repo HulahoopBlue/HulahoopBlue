85a79ab70c901f449cd368b123a1ad3c5bc8121c
SMMac <seungmin6385@gmail.com>
2025-09-19 18:05:10 +0900

    feature-white-lsm-0919-v1.0

--- white/src/main/java/com/hulahoopblue/white/WhiteApplication.java
+++ white/src/main/java/com/hulahoopblue/white/WhiteApplication.java
@@ -1,9 +1,14 @@
 package com.hulahoopblue.white;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.hulahoopblue.white")
+@MapperScan(basePackages = "com.hulahoopblue.white", annotationClass = Mapper.class)
 public class WhiteApplication {
 
     public static void main(String[] args) {
--- white/src/main/java/com/hulahoopblue/white/member/model/service/MemberService.java
+++ white/src/main/java/com/hulahoopblue/white/member/model/service/MemberService.java
@@ -1,24 +1,43 @@
 package com.hulahoopblue.white.member.model.service;
 
-import com.hulahoopblue.white.member.model.dao.MemberDAO;
+import com.hulahoopblue.white.member.model.dao.MemberMapper;
 import com.hulahoopblue.white.member.model.dto.MemberDTO;
-import com.hulahoopblue.white.SHA256Util;
+import com.hulahoopblue.white.merchant.model.dto.MerchantDTO;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class MemberService {
 
-    private final MemberDAO memberMapper;
+    private final MemberMapper memberMapper;
 
-    public MemberService(MemberDAO memberMapper) { this.memberMapper = memberMapper; }
+    public MemberService(MemberMapper memberMapper) {
+        this.memberMapper = memberMapper;
+    }
 
-    public List<MemberDTO> findAllMember() { return memberMapper.findAllMember(); }
+    // 전체 회원 조회
+    public List<MemberDTO> getAllMembers() {
+        return memberMapper.selectAllMembers();
+    }
 
-    // 로그인용 조회
+    // 조건별 회원 조회
+    public List<MemberDTO> getMembers(String categoryCd, String merchantNum, String searchText) {
+        return memberMapper.selectMembersByCriteria(categoryCd, merchantNum, searchText);
+    }
+
+    // ID로 회원 조회
     public MemberDTO findById(String id) {
         return memberMapper.selectMemberById(id);
+    }
+
+    // 가맹점 목록 조회 (MemberController API용)
+    public List<MerchantDTO> getMerchantList() {
+        return memberMapper.selectMerchantList();
+    }
+
+    // 카테고리 목록 조회
+    public List<String> getCategoryList() {
+        return memberMapper.selectCategoryList();
     }
 }
