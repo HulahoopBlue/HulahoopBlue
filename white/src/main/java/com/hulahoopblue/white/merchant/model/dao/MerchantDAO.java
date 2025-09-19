4c5f8fb020c8449b8048737f7c9f0bdd7dc8a25b
Lee-KyungChul <backpfly@gmail.com>
2025-09-19 10:50:49 +0900

    0919-lkc

--- white/src/main/java/com/hulahoopblue/white/merchant/model/dao/MerchantDAO.java
+++ white/src/main/java/com/hulahoopblue/white/merchant/model/dao/MerchantDAO.java
@@ -1,4 +1,14 @@
 package com.hulahoopblue.white.merchant.model.dao;
 
-public class MerchantDAO {
+import com.hulahoopblue.white.merchant.model.dto.MerchantDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface MerchantDAO {
+    List<MerchantDTO> findAllMerchants();
+    List<MerchantDTO> findMerchantsByCriteria(@Param("params") Map<String, String> params);
 }
