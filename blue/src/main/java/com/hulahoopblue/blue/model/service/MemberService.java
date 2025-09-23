eec8a3ec75b469354ba3e9416e7c9cb8718c46cc
BOOK-BVBUIR6SCP\amk02 <amk0223@naver.com>
2025-09-23 16:57:04 +0900

    20250923 blue

--- blue/src/main/java/com/hulahoopblue/blue/model/service/MemberService.java
+++ blue/src/main/java/com/hulahoopblue/blue/model/service/MemberService.java
@@ -25,12 +25,12 @@ public class MemberService {
     }
 
     @Transactional
-    public void updateMember(MemberDTO updateMember) {
+    public void updateNewMember(MemberDTO updateMember) {
         memberMapper.updateNewMember(updateMember);
     }
 
     @Transactional
-    public void deleteMember(MemberDTO newMember) { memberMapper.deleteNewMember(newMember); }
+    public void deleteNewMember(MemberDTO newMember) { memberMapper.deleteNewMember(newMember); }
 
     // 로그인용 조회
     public MemberDTO findById(String id) {
